809c7dd6256ce9335a613f495676dd8fd0086200
lvmaozilink <2468852146@qq.com>
2020-06-20 17:34:45 +0800

    智能农业

--- 智能农业/src/com/example/wangpengfei/dialog/SetAir_ThresholdValue.java
+++ 智能农业/src/com/example/wangpengfei/dialog/SetAir_ThresholdValue.java
@@ -1,44 +1,85 @@
 package com.example.wangpengfei.dialog;
 
-
+import com.example.wangpengfei.ClientApp;
 import com.example.wangpengfei.R;
+import com.example.wangpengfei.bean.SensorConfigBean;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 @SuppressLint("ValidFragment")
-public class SetAir_ThresholdValue extends BaseDialog{
+public class SetAir_ThresholdValue extends BaseDialog {
 
 	Context mcontext;
-	
-	
+
+	EditText airTempMinEt, airTempMaxEt, airHumiMinEt, airHumiMaxEt;
+	Button airOkBtn;
+
+	ClientApp mApp;
+
 	public SetAir_ThresholdValue(Context mcontect) {
 		super(mcontect);
-		this.mcontext=mcontect;
-		
+		this.mcontext = mcontect;
+		mApp = (ClientApp) this.getActivity().getApplication();
 
 	}
-
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 
 		super.onActivityCreated(savedInstanceState);
+		InitView();
+		setListener();
 	}
 
+	private void setListener() {
+		airOkBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				SensorConfigBean mBean=mApp.getSensorConfig();
+				mBean.minAirTemperature=Integer.parseInt(airTempMinEt.getText().toString().trim().equals("")? "0":airTempMinEt.getText().toString().trim());
+				mBean.maxAirTemperature=Integer.parseInt(airTempMaxEt.getText().toString().trim().equals("")? "0":airTempMaxEt.getText().toString().trim());
+				mBean.minAirHumidity=Integer.parseInt(airHumiMinEt.getText().toString().trim().equals("")? "0":airHumiMinEt.getText().toString().trim());
+				mBean.maxAirHumidity=Integer.parseInt(airHumiMaxEt.getText().toString().trim().equals("")? "0":airHumiMaxEt.getText().toString().trim());
+				mApp.saveSensorConfig(mBean);
+			
+			
+			
+			}
+		});
+		
+
+		
+	}
+
+	private void InitView() {
+		airOkBtn = (Button) getView().findViewById(R.id.air_setBtn);
+		airTempMinEt = (EditText) getView().findViewById(
+				R.id.major_min_value_edit_text);
+		airTempMaxEt = (EditText) getView().findViewById(
+				R.id.major_max_value_edit_text);
+		airHumiMinEt = (EditText) getView().findViewById(
+				R.id.slave_min_value_edit_text);
+		airHumiMaxEt = (EditText) getView().findViewById(
+				R.id.slave_max_value_edit_text);
+
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-//		return super.onCreateView(inflater, container, savedInstanceState);
-	return inflater.inflate(R.layout.set_air_temp_humi_layout, container, false);
-	
+		// return super.onCreateView(inflater, container, savedInstanceState);
+		return inflater.inflate(R.layout.set_soil_temp_humi_layout, container,
+				false);
+
 	}
-	
-	
 
 }
--- 智能农业/src/com/example/wangpengfei/dialog/SetSoil_ThresholdValue.java
+++ 智能农业/src/com/example/wangpengfei/dialog/SetSoil_ThresholdValue.java
@@ -1,34 +1,68 @@
 package com.example.wangpengfei.dialog;
 
+import com.example.wangpengfei.ClientApp;
 import com.example.wangpengfei.R;
+import com.example.wangpengfei.bean.SensorConfigBean;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 @SuppressLint("ValidFragment")
 public class SetSoil_ThresholdValue extends BaseDialog{
 	Context mContext;
-
+	EditText soilTempMinEt,soilTempMaxEt,soilHumiMinEt,soilHumiMaxEt;
+	Button soilOkBtn;
+	ClientApp mApp;
 	public SetSoil_ThresholdValue(Context mcontect) {
 		super(mcontect);
 		this.mcontext=mcontect;
-		
+		mApp = (ClientApp) this.getActivity().getApplication();
+
 	}
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
+		InitView();
+		setListener();
+	}
+
+	private void setListener() {
+		soilOkBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				SensorConfigBean mBean=mApp.getSensorConfig();
+				mBean.minSoilTemperature=Integer.parseInt(soilTempMinEt.getText().toString().trim().equals("")? "0":soilTempMinEt.getText().toString().trim());
+				mBean.maxSoilTemperature=Integer.parseInt(soilTempMaxEt.getText().toString().trim().equals("")? "0":soilTempMaxEt.getText().toString().trim());
+				mBean.minSoilHumidity=Integer.parseInt(soilHumiMinEt.getText().toString().trim().equals("")? "0":soilHumiMinEt.getText().toString().trim());
+				mBean.maxSoilHumidity=Integer.parseInt(soilHumiMaxEt.getText().toString().trim().equals("")? "0":soilHumiMaxEt.getText().toString().trim());
+				mApp.saveSensorConfig(mBean);
+			}
+		});
+		
+		
+	}
+
+	private void InitView() {
+		soilOkBtn=(Button)getView().findViewById(R.id.soil_setBtn);
+		soilTempMinEt=(EditText)getView().findViewById(R.id.soil_temp_min_value_edit_text);
+		soilTempMaxEt=(EditText)getView().findViewById(R.id.soil_temp_max_value_edit_text);
+		soilHumiMinEt=(EditText)getView().findViewById(R.id.soil_humi_min_value_edit_text);
+		soilHumiMaxEt=(EditText)getView().findViewById(R.id.soil_humi_max_value_edit_text);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+
 //		return super.onCreateView(inflater, container, savedInstanceState);
 		return inflater.inflate(R.layout.set_soil_temp_humi_layout, container, false);
 		
